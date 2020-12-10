d109398ad2686c2c6fa3864580b1d08fc743b154
Mercovsk <marcbaja08@gmail.com>
2020-12-10 22:31:02 +0800

    edit vigenere

--- vigenere.py
+++ vigenere.py
@@ -6,45 +6,35 @@ class Vigenere:
         self.key_code = key_code
 
     def encrypt(self, plain_text):
-        encrypted_text = []
-        counter = 0
+        encrypted_text = ''
 
-        for letter in plain_text.upper():
-            if counter == len(self.key_code):
-                counter = 0
+        if plain_text == ' ':
+            encrypted_text = plain_text
 
-            if letter == ' ':
-                encrypted_text.append(letter)
-            elif letter == ',':
-                encrypted_text.append(letter)
-            else:
-                number = ord(letter) + self.key_code[counter]
-                if number > 90:
-                    number -= 26
-                encrypted_text.append(chr(number))
-                counter += 1
-        msg = ""
-        return msg.join(encrypted_text)
+        elif plain_text == ',':
+            encrypted_text = plain_text
+
+        else:
+            number = ord(plain_text.upper()) + self.key_code
+            if number > 90:
+                number -= 26
+            encrypted_text = chr(number)
+
+        return encrypted_text
 
     def decrypt(self, plain_text):
-        decrypted_text = []
-        counter = 0
+        decrypted_text = ''
 
-        for letter in plain_text:
-            if counter == len(self.key_code):
-                counter = 0
+        if plain_text == ' ':
+            decrypted_text = plain_text
 
-            if letter == ' ':
-                decrypted_text.append(letter)
-            elif letter == ',':
-                decrypted_text.append(letter)
-            else:
-                number = ord(letter) - self.key_code[counter]
-                print(number)
-                if number < 65:
-                    number += 26
-                decrypted_text.append(chr(number))
-                counter += 1
+        elif plain_text == ',':
+            decrypted_text = plain_text
 
-        msg = ''
-        return msg.join(decrypted_text)
+        else:
+            number = ord(plain_text.upper()) - self.key_code
+            if number < 64:
+                number += 26
+            decrypted_text = chr(number)
+    
+        return decrypted_text
